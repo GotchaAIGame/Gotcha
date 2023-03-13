8b3db531b67ad5c66b98f09a6f05f6a436227b2b
박다솜 <dev.somda@gmail.com>
2023-03-13 14:59:21 +0900

    ✨ feat: button components 추가

---
+++ frontend/src/Components/common/Button.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import classNames from "classnames";
+import "./Button.scss";
+
+interface BtnProps {
+  text?: string;
+  size?: string;
+  color?: string;
+  onClick?: any; // 함수 인자 및 return 값 특정 불가
+}
+
+function Button({ text, size, color, onClick }: BtnProps) {
+  const handleClick = () => onClick();
+
+  return (
+    <button
+      type="button"
+      className={classNames("commonButton", size, color)}
+      onClick={onClick && handleClick}
+    >
+      {text}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  text: "",
+  size: "large",
+  color: "lime",
+  onClick: () => {
+    return null;
+  },
+};
+
+export default Button;
