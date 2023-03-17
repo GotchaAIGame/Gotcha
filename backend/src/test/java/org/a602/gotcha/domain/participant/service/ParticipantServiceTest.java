7a0b183edcde2276c5baa07c82c7e5e0dace8eda
yezi <yejixheo@gmail.com>
2023-03-17 17:55:38 +0900

    ✅ test : add duplicate participant nickname test and remove whole previous code
    - 기존 서비스 테스트 코드가 repository와의 의존관계가 있어, 이를 끊어 순수 service layer만 테스트할 수 있도록 수정.

--- backend/src/test/java/org/a602/gotcha/domain/participant/service/ParticipantServiceTest.java
+++ backend/src/test/java/org/a602/gotcha/domain/participant/service/ParticipantServiceTest.java
@@ -1,227 +1,90 @@
 package org.a602.gotcha.domain.participant.service;
 
-import org.a602.gotcha.domain.member.entity.Member;
-import org.a602.gotcha.domain.member.repository.MemberRepository;
 import org.a602.gotcha.domain.participant.entity.Participant;
 import org.a602.gotcha.domain.participant.exception.DuplicateNicknameException;
-import org.a602.gotcha.domain.participant.exception.ParticipantLoginFailedException;
-import org.a602.gotcha.domain.participant.exception.ParticipantNotFoundException;
 import org.a602.gotcha.domain.participant.repository.ParticipantRepository;
-import org.a602.gotcha.domain.participant.request.ParticipantCheckRequest;
-import org.a602.gotcha.domain.participant.request.ParticipantGameStartRequest;
-import org.a602.gotcha.domain.participant.response.ParticipantInfoResponse;
+import org.a602.gotcha.domain.participant.request.DuplicateNicknameRequest;
 import org.a602.gotcha.domain.room.entity.Room;
 import org.a602.gotcha.domain.room.exception.RoomNotFoundException;
 import org.a602.gotcha.domain.room.repository.RoomRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class ParticipantServiceTest {
 
-    @Autowired
+    @InjectMocks
     private ParticipantService participantService;
-    @Autowired
+
+    @Mock
     private ParticipantRepository participantRepository;
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
+    @Mock
     private RoomRepository roomRepository;
 
-    Long ROOM_ID;
-    Long PARTICIPANT_ID;
-    String USER_NICKNAME = "yezi";
-    String USER_PWD = "1234";
-    Boolean USER_IS_FINISHED = false;
-
-    @BeforeEach
-    void setGameRoom() {
-
-        // 멤버 생성
-        Member member = new Member("yezi", "1234", "SSAFY", "yezi@ssafy.com", "일반");
-        memberRepository.save(member);
-
-        // 게임방 생성
-        Room room = Room.builder()
-                .color("Blue")
-                .code("GAMEROOM")
-                .logoUrl("ssafy.com")
-                .eventUrl("ssafy.com")
-                .title("새로운 게임입니다.")
-                .eventDesc("연습용 게임입니다")
-                .startTime(LocalDateTime.parse("2023-03-01T09:00:00"))
-                .endTime(LocalDateTime.parse("2023-03-07T18:00:00"))
-                .hasReward(false)
-                .rewardDesc("보상 설명")
-                .member(member).build();
-        Room savedRoom = roomRepository.save(room);
-        ROOM_ID = savedRoom.getId();
-        // 참여자 생성
-        Participant participant = Participant.builder()
-                .nickname(USER_NICKNAME)
-                .password(USER_PWD)
-                .room(room)
-                .isFinished(USER_IS_FINISHED)
-                .build();
-        Participant savedParticipant = participantRepository.save(participant);
-        PARTICIPANT_ID = savedParticipant.getId();
-
-    }
+    Long ROOM_ID = 1L;
+    Long INVALID_ROOM_ID = 10L;
+    String USER_NICKNAME = "YEZI";
+    String NOT_REGISTERED_NICKNAME = "TAEGYU";
 
     @Nested
-    @DisplayName("참여자 등록하기 메소드는")
-    class RegisterUser {
+    @DisplayName("참여자 닉네임 중복 체크 메소드는")
+    class DuplicateCheck {
 
         @Test
-        @DisplayName("찾는 방이 없으면 RoomNotFound 예외 발생")
-        void notValidCode() {
+        @DisplayName("방 정보가 없을 경우 RoomNotFoud 예외 발생")
+        void notValidRoomId() {
             // given
-            ParticipantCheckRequest request = ParticipantCheckRequest.builder()
-                    .roomId(1000000L)
-                    .nickname("yezi")
-                    .password("1111")
+            DuplicateNicknameRequest request = DuplicateNicknameRequest.builder()
+                    .roomId(INVALID_ROOM_ID)
+                    .nickname(NOT_REGISTERED_NICKNAME)
                     .build();
+            // when
+            when(roomRepository.findById(eq(INVALID_ROOM_ID))).thenReturn(Optional.empty());
             // then
-            assertThrows(RoomNotFoundException.class, ()
-                    -> participantService.registerUser(request));
+            assertThrows(RoomNotFoundException.class, () -> participantService.existDuplicateNickname(request));
         }
 
         @Test
-        @DisplayName("이미 등록된 사용자면 DuplicateNickname 예외 발생")
-        void registerWithExistedNickname() {
+        @DisplayName("해당하는 방에 이미 동일 닉네임이 있다면 DuplicateNickname 예외 발생")
+        void existDuplicatedNickname() {
             // given
-            ParticipantCheckRequest request = ParticipantCheckRequest.builder()
+            DuplicateNicknameRequest request = DuplicateNicknameRequest.builder()
                     .roomId(ROOM_ID)
                     .nickname(USER_NICKNAME)
-                    .password(USER_PWD)
                     .build();
-            // then
-            assertThrows(DuplicateNicknameException.class, ()
-                    -> participantService.registerUser(request));
+            //when
+            when(roomRepository.findById(any(Long.class))).thenReturn(Optional.of(Room.builder().build()));
+            when(participantRepository.findParticipantByRoomIdAndNickname(ROOM_ID, USER_NICKNAME))
+                    .thenReturn(Optional.of(Participant.builder().build()));
+            assertThrows(DuplicateNicknameException.class, () -> participantService.existDuplicateNickname(request));
         }
 
         @Test
-        @DisplayName("룸Id가 유효하고 중복된 닉네임이 없다면 유저 등록 성공")
-        void registerUserSuccessfully() {
+        @DisplayName("해당하는 방에 동일 닉네임이 없다면 false 값 반환")
+        void passDuplicateNickname() {
             // given
-            ParticipantCheckRequest request = ParticipantCheckRequest.builder()
+            DuplicateNicknameRequest request = DuplicateNicknameRequest.builder()
                     .roomId(ROOM_ID)
-                    .nickname("Taegyu")
-                    .password("1234")
+                    .nickname(NOT_REGISTERED_NICKNAME)
                     .build();
-            // when
-            Participant savedUser = participantService.registerUser(request);
-            // then
-            assertEquals("Taegyu", savedUser.getNickname());
-            assertEquals("1234", savedUser.getPassword());
-        }
-
-    }
-
-    @Nested
-    @DisplayName("기존 참여자 로그인 메소드는")
-    class LoginUser {
-
-        @Test
-        @DisplayName("해당 방에 찾는 닉네임이 없을 경우 ParticipantNotFound 예외 발생")
-        void loginWithInvalidNickname() {
-            // given
-            ParticipantCheckRequest request = ParticipantCheckRequest.builder()
-                    .roomId(ROOM_ID)
-                    .nickname("Taegyu")
-                    .password("1234")
-                    .build();
-            // then
-            assertThrows(ParticipantNotFoundException.class, ()
-                    -> participantService.getUserInfo(request));
-        }
-
-        @Test
-        @DisplayName("닉네임과 비밀번호가 불일치하면 ParticipantLoginFailed 예외 발생")
-        void loginFailed() {
-            // given
-            ParticipantCheckRequest request = ParticipantCheckRequest.builder()
-                    .roomId(ROOM_ID)
-                    .nickname(USER_NICKNAME)
-                    .password("111111111")
-                    .build();
-            // then
-            assertThrows(ParticipantLoginFailedException.class, ()
-                    -> participantService.getUserInfo(request));
-        }
-
-        @Test
-        @DisplayName("모든 검증이 끝나면 유저가 해당게임에 대한 정보 리턴")
-        void loginSuccess() {
-            // given
-            ParticipantCheckRequest request = ParticipantCheckRequest.builder()
-                    .roomId(ROOM_ID)
-                    .nickname(USER_NICKNAME)
-                    .password(USER_PWD)
-                    .build();
-            // when
-            ParticipantInfoResponse response = participantService.getUserInfo(request);
-            // then
-            assertEquals(USER_IS_FINISHED, response.getIsFinished());
-        }
-
-    }
-
-    @Nested
-    @DisplayName("시작 시간 업데이트 메소드는")
-    class UpdateStartTime {
-
-        @Test
-        @DisplayName("방, 닉네임이 일치하면 제공 받은 시간으로 업데이트 한다.")
-        void updateStartInfo() {
-            // given
-            LocalDateTime startTime = LocalDateTime.of(2023, 3, 13, 10, 2, 30);
-            ParticipantGameStartRequest request = ParticipantGameStartRequest.builder()
-                    .roomId(ROOM_ID)
-                    .nickname(USER_NICKNAME)
-                    .startTime(startTime)
-                    .build();
-            // when
-            participantService.updateStartTime(request);
-            Participant user = participantRepository.findById(PARTICIPANT_ID)
-                    .orElseThrow(ParticipantNotFoundException::new);
-            // then
-            assertEquals(startTime, user.getStartTime());
-        }
-    }
-
-    @Nested
-    @DisplayName("유저 유효성 체크 메소드는")
-    class CheckUserValidation {
-
-        @Test
-        @DisplayName("방에 해당하는 유저가 없으면 false 반환")
-        void userNotFound() {
-            // given
-            String nickname = "Taegyu";
-            // when
-            boolean result = participantService.checkUserValidation(ROOM_ID, nickname);
-            // then
-            assertFalse(result);
-        }
-
-        @Test
-        @DisplayName("방에 해당하는 유저가 있으면 true 반환")
-        void validUser() {
-            // when
-            boolean result = participantService.checkUserValidation(ROOM_ID, USER_NICKNAME);
-            // then
-            assertTrue(result);
+            //when
+            when(roomRepository.findById(any(Long.class))).thenReturn(Optional.of(Room.builder().build()));
+            when(participantRepository.findParticipantByRoomIdAndNickname(ROOM_ID, NOT_REGISTERED_NICKNAME))
+                    .thenReturn(Optional.empty());
+            assertFalse(participantService.existDuplicateNickname(request));
         }
 
     }
