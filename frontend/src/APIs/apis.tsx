97ec69145d36b3793ca73e8f2fb0ebd6f38e703f
veonico <veonico@naver.com>
2023-03-17 10:10:22 +0900

    ✨ feat : register, start API implemented

--- frontend/src/APIs/apis.tsx
+++ frontend/src/APIs/apis.tsx
@@ -4,6 +4,22 @@ import request from "./agents";
 const gamePlayAPI = {
   enter: (roomCode: number): Promise<object> =>
     request.get("/api/game/enter", { params: { roomCode } }),
+  register: (
+    roomId: number,
+    nickname: string,
+    password: number
+  ): Promise<Object> =>
+    request.post("api/game/register", {
+      data: { roomId, nickname, password },
+    }),
+  start: (
+    roomId: number,
+    nickname: string,
+    startDateTime: string
+  ): Promise<Object> =>
+    request.post("api/game/start", {
+      data: { roomId, nickname, startDateTime },
+    }),
 };
 
 export { gamePlayAPI };
