c019f090abfa85fc5a1ddd071837a6f6c5868e38
박다솜 <dev.somda@gmail.com>
2023-03-20 17:42:31 +0900

    🚧 in progress: image localstorage 저장 기능 작업 중

--- frontend/src/Components/CreateGame/GameCard.tsx
+++ frontend/src/Components/CreateGame/GameCard.tsx
@@ -5,9 +5,23 @@ export default function GameCard() {
   const uploadImage = useRef<HTMLInputElement>(null);
 
   const uploadHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    console.log(target.value);
     const files = uploadImage.current?.files;
     if (files && files.length > 0) {
       setInputImage(URL.createObjectURL(files[0]));
+      const reader = new FileReader();
+      console.log(reader.readAsDataURL(files[0]));
+      localStorage.setItem("image", target.value);
+      console.log(URL.createObjectURL(files[0]));
+    }
+  };
+
+  const checkLocal = () => {
+    const localImg = localStorage.getItem("image");
+    console.log(localImg);
+    if (localImg) {
+      setInputImage(localImg);
     }
   };
 
