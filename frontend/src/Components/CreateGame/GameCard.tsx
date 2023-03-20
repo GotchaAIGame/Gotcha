0e7a2c8f9188fde41d27c79d993a8cb9c7cdb5f5
박다솜 <dev.somda@gmail.com>
2023-03-20 16:48:39 +0900

    ✨ feat: component 추가가 가능한 buton 생성

--- frontend/src/Components/CreateGame/GameCard.tsx
+++ frontend/src/Components/CreateGame/GameCard.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 export default function GameCard() {
+  const [inputImage, setInputImage] = useState<string>("");
+  const uploadImage = useRef<HTMLInputElement>(null);
+
+  const uploadHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = uploadImage.current?.files;
+    if (files && files.length > 0) {
+      setInputImage(URL.createObjectURL(files[0]));
+    }
+  };
+
   return (
     <div className="card-wrapper">
       <header>
         <input type="text" placeholder="문제이름" />
       </header>
-      <div className="file-input-wrapper">
-        <input type="file" accept="image/*" />
-      </div>
+      {inputImage ? (
+        <div className="upload-img-wrapper">
+          <img src={inputImage} alt="" />
+        </div>
+      ) : (
+        <div className="file-input-wrapper">
+          <input
+            type="file"
+            accept="image/*"
+            onChange={uploadHandler}
+            ref={uploadImage}
+          />
+        </div>
+      )}
       <div className="hint-text-box">힌트</div>
       <div className="hint-input-wrapper">
         <input type="text" placeholder="힌트를 추가해 주세요" />
--- frontend/src/Components/CreateGame/GameCardCarousel.tsx
+++ frontend/src/Components/CreateGame/GameCardCarousel.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import GameCard from "./GameCard";
 
 export default function GameCardCarousel() {
+  const [problemCount, setProblemCount] = useState<number>(1);
+
+  const problemCountArr = Array.from({ length: problemCount }, (_, i) => i);
+
+  const problemCountHandler = () => {
+    setProblemCount(problemCount + 1);
+  };
+
   return (
     <div>
-      <GameCard />
-      <GameCard />
+      {problemCountArr.map((index) => (
+        <GameCard key={index} />
+      ))}
+      <button
+        type="button"
+        onClick={problemCountHandler}
+        className="add-img-button"
+      >
+        +
+      </button>
     </div>
   );
 }
