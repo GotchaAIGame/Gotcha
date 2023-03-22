a4c4fb3b5133a943056e7202414acb2df938d3cc
gimgyuyeon <8080claire@naver.com>
2023-03-22 13:57:28 +0900

    ✨ feat: CustomNav 추가

--- frontend/src/Components/common/CustomNavbar.tsx
+++ frontend/src/Components/common/CustomNavbar.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import logo from "@assets/logo.svg";
 import "./styles/CustomNavbar.scss";
+import classNames from "classnames";
 
 export default function CustomNavbar() {
   const themeColor = useSelector((state: any) => state.theme.themeColor);
   const themeLogo = useSelector((state: any) => state.theme.themeLogo);
+  const themeTitle = useSelector((state: any) => state.theme.themeTitle);
+
+  const bgColor = {
+    backgroundColor: themeColor,
+  };
 
   return (
-    <header className="custom-nav-container">
-      <h1>커스텀네브지롱</h1>
-      <button type="button" style={{ backgroundColor: themeColor }}>
-        테스트중입니다.
-      </button>
+    <header className="custom-nav-container" style={bgColor}>
       <img src={themeLogo} alt="로고" />
+      <h3 className="custom-title-wrapper">{themeTitle}</h3>
     </header>
   );
 }
--- frontend/src/stores/users/themeSlice.tsx
+++ frontend/src/stores/users/themeSlice.tsx
@@ -4,11 +4,13 @@ import logo from "@assets/logo.svg";
 interface ThemeState {
   themeColor: string;
   themeLogo: string;
+  themeTitle: string;
 }
 
 const initialState: ThemeState = {
   themeColor: "#5551FF",
   themeLogo: logo,
+  themeTitle: "일이삼사오육칠팔구십일이삼사오육칠팔구십"
 };
 
 export const themeSlice = createSlice({
@@ -17,13 +19,18 @@ export const themeSlice = createSlice({
   reducers: {
     setTheme: (
       state,
-      action: PayloadAction<{ themeColor: string; themeLogo: string }>
+      action: PayloadAction<{
+        themeColor: string;
+        themeLogo: string;
+        themeTitle: string;
+      }>
     ) => {
-      const { themeColor, themeLogo } = action.payload;
+      const { themeColor, themeLogo, themeTitle } = action.payload;
       return {
         ...state,
         themeColor,
         themeLogo,
+        themeTitle,
       };
     },
   },
