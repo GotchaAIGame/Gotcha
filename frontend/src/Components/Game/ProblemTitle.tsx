4678fd31626c3d8651a856c2eb025bd13c83b92b
veonico <veonico@naver.com>
2023-03-23 11:52:08 +0900

    ✨ feat : GamePage implemented

--- frontend/src/Components/Game/ProblemTitle.tsx
+++ frontend/src/Components/Game/ProblemTitle.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import questionButton from "@assets/questionButton.svg";
 
 function ProblemTitle() {
+  const problemTitle = "월계를 찾아라! 월계!";
   return (
-    <div>
-      <h1> 문제 타이틀 </h1>
+    <div className="problem-title-wrapper">
+      <img
+        src={questionButton}
+        alt="questionButton"
+        className="img-questionButtom"
+      />
+      <h5> {problemTitle} </h5>
     </div>
   );
 }
