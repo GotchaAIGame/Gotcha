c861f19e2906706cd7610f90c21e2274c3ec5445
박다솜 <twsevn@gmail.com>
2023-03-24 10:07:23 +0900

    hotfix: directory 삭제 및 수정

--- frontend/src/components/CreatorMain/BeforeStartedGames.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function BeforeStartedGames() {
-  return (
-    <div>BeforeStartedGames</div>
-  )
-}
--- frontend/src/components/CreatorMain/FinishedGames.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FinishedGames() {
-  return (
-    <div>FinishedGames</div>
-  )
-}
