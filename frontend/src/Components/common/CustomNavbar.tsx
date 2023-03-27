8f8b30e9d5a65a6441e6c504745a800c00678ed2
gimgyuyeon <8080claire@naver.com>
2023-03-27 10:05:37 +0900

    ✨ feat: 이벤트 참여하기 모달 생성

--- frontend/src/Components/common/CustomNavbar.tsx
+++ frontend/src/Components/common/CustomNavbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import "./styles/CustomNavbar.scss";
+import { Link } from "react-router-dom";
 
 export default function CustomNavbar() {
   const themeColor = useSelector((state: any) => state.theme.themeColor);
@@ -13,7 +14,9 @@ export default function CustomNavbar() {
 
   return (
     <header className="custom-nav-container" style={bgColor}>
-      <img src={themeLogo} alt="로고" />
+      <Link to="/">
+        <img src={themeLogo} alt="로고" />
+      </Link>
       <h3 className="custom-title-wrapper">{themeTitle}</h3>
     </header>
   );
