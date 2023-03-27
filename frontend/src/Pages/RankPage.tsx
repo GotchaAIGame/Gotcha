2dd85af84c5083ba1ba174f69213e44617063729
gimgyuyeon <8080claire@naver.com>
2023-03-27 11:28:51 +0900

    ✨ feat: RankPage confetti 추가

--- frontend/src/Pages/RankPage.tsx
+++ frontend/src/Pages/RankPage.tsx
@@ -2,12 +2,34 @@ import React from "react";
 import PlayerRank from "@components/RankPage/PlayerRank";
 import CustomNavbar from "@components/common/CustomNavbar";
 import RankButtons from "@components/RankPage/RankButtons";
+import Confetti from "react-confetti";
 
 export default function RankPage() {
   return (
     <div>
       <CustomNavbar />
       <div className="rank-page-container">
+        <Confetti
+          numberOfPieces={100}
+          gravity={0.01}
+          colors={[
+            "#FF8577",
+            "#e91e63",
+            "#9c27b0",
+            "#C7B9FF",
+            "#5551FF",
+            "#699BF7",
+            "#CFE",
+            "#00bcd4",
+            "#37A954",
+            "#4caf50",
+            "#8BC34A",
+            "#8bc34a",
+            "#DFF93E",
+            "#ffeb3b",
+            "#FFC700",
+          ]}
+        />
         <PlayerRank />
         <RankButtons />
       </div>
