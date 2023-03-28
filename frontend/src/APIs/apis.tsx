e06b505688a095bed7792479556b72c086352673
veonico <veonico@naver.com>
2023-03-28 10:05:46 +0900

    ⚡️ update : api url modified

--- frontend/src/APIs/apis.tsx
+++ frontend/src/APIs/apis.tsx
@@ -4,13 +4,13 @@ import request from "./agents";
 // APis about game play
 const gamePlayAPI = {
   enter: (roomCode: number): Promise<AxiosResponse> =>
-    request.get("/api/game/enter", { params: { roomCode } }),
+    request.get("game/enter", { params: { roomCode } }),
   register: (
     roomId: number,
     nickname: string,
     password: number
   ): Promise<AxiosResponse> =>
-    request.post("api/game/register", {
+    request.post("game/register", {
       data: { roomId, nickname, password },
     }),
   start: (
@@ -18,16 +18,16 @@ const gamePlayAPI = {
     nickname: string,
     startDateTime: string
   ): Promise<AxiosResponse> =>
-    request.post("api/game/start", {
+    request.post("game/start", {
       data: { roomId, nickname, startDateTime },
     }),
 };
 
 const memberAPI = {
-  duplicateEmail : (email:string):
-  Promise<AxiosResponse> => request.get("member/duplicateEmail", {
-    params : {email},
-  })
-}
+  duplicateEmail: (email: string): Promise<AxiosResponse> =>
+    request.get("member/duplicateEmail", {
+      params: { email },
+    }),
+};
 
 export { gamePlayAPI, memberAPI };
--- frontend/src/components/MainPage/InputPinNum.tsx
+++ frontend/src/components/MainPage/InputPinNum.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@mui/material";
 
 export default function InputPinNum() {
   return (
     <Grid container className="input-pin-num-container">
       <Grid item md={9} sm={9}>
-        <input type="text" placeholder="PIN번호" />
+        <form>
+          <input type="text" placeholder="PIN번호" />
+        </form>
       </Grid>
     </Grid>
   );
