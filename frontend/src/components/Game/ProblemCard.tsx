c7942a96ac156c00b5890ddda3199bfdcd185173
veonico <veonico@naver.com>
2023-03-29 14:18:59 +0900

    🐛 fix : image editor overlay moved

--- frontend/src/components/Game/ProblemCard.tsx
+++ frontend/src/components/Game/ProblemCard.tsx
@@ -31,36 +31,39 @@ function ProblemCard(props: problemProps) {
   };
 
   return (
-    <div className="outer-card-wrapper">
-      <div className="problem-title-container">
-        <h5>{problemName}</h5>
-      </div>
-      <div className="inner-card-container">
-        <div className="original-image-container">
-          <img src={problemImgURL} alt={problemName} />
+    <>
+      <div className="outer-card-wrapper">
+        <div className="problem-title-container">
+          <h5>{problemName}</h5>
         </div>
-        <div className="input-image-container">
-          {hasImage ? (
-            <img
-              src="https://user-images.githubusercontent.com/47023884/225485195-f44d038c-a859-436c-ba1a-fb27c7414062.png"
-              alt="yuegui"
-            />
-          ) : (
-            <form className="empty-image-container">
-              <label htmlFor="upload">
-                <p className="plus"> + </p>
-                <h5 id="take-pic"> 사진 찍기 </h5>
-                <input
-                  id="upload"
-                  type="file"
-                  accept="image/*"
-                  ref={uploadImage}
-                  onChange={uploadHandler}
-                  className="invisible"
-                />
-              </label>
-            </form>
-          )}
+        <div className="inner-card-container">
+          <div className="original-image-container">
+            <img src={problemImgURL} alt={problemName} />
+          </div>
+
+          <div className="input-image-container">
+            {hasImage ? (
+              <img
+                src="https://user-images.githubusercontent.com/47023884/225485195-f44d038c-a859-436c-ba1a-fb27c7414062.png"
+                alt="yuegui"
+              />
+            ) : (
+              <form className="empty-image-container">
+                <label htmlFor="upload">
+                  <p className="plus"> + </p>
+                  <h5 id="take-pic"> 사진 찍기 </h5>
+                  <input
+                    id="upload"
+                    type="file"
+                    accept="image/*"
+                    ref={uploadImage}
+                    onChange={uploadHandler}
+                    className="invisible"
+                  />
+                </label>
+              </form>
+            )}
+          </div>
         </div>
       </div>
       {open && (
@@ -68,34 +71,39 @@ function ProblemCard(props: problemProps) {
           <div
             className="image-editor-overlay"
             onClick={() => {
-              console.log("하하");
               return setOpen(false);
             }}
+            onKeyDown={() => setOpen(false)}
             role="presentation"
-          >
-            <div className="image-editor-content">
-              <div className="image-editor-image">
-                <Cropper
-                  src={Image}
-                  style={{
-                    maxHeight: "70vh",
-                    maxWidth: "80vw",
-                    overflow: "auto",
-                  }}
-                  background
-                  minCropBoxHeight={100}
-                  minCropBoxWidth={100}
-                  viewMode={1}
-                />
-              </div>
-              <div className="image-editor-buttons">
-                <Button text="제출하기" color="skyblue" />
-              </div>
+          />
+          <div className="image-editor-content">
+            <div className="image-editor-image">
+              <Cropper
+                src={Image}
+                style={{
+                  maxHeight: "60vh",
+                  maxWidth: "90vw",
+                  // overflow: "auto",
+                }}
+                minCropBoxHeight={100}
+                minCropBoxWidth={100}
+                viewMode={1}
+                background={false}
+              />
+            </div>
+            <div className="image-editor-buttons">
+              <Button
+                text="제출하기"
+                color="skyblue"
+                onClick={() => {
+                  console.log("안녕하세요");
+                }}
+              />
             </div>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
