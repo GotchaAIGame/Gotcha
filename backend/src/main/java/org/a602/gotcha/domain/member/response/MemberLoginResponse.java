b075176aa330beb55a0d5c877e7d48701a398f52
alkwen0996 <alkwen0996@naver.com>
2023-03-30 17:12:17 +0900

    ♻️refactor: dto notnull, schema 추가

--- backend/src/main/java/org/a602/gotcha/domain/member/response/MemberLoginResponse.java
+++ backend/src/main/java/org/a602/gotcha/domain/member/response/MemberLoginResponse.java
@@ -1,19 +1,38 @@
 package org.a602.gotcha.domain.member.response;
 
+import javax.validation.constraints.NotNull;
+
 import org.a602.gotcha.domain.member.entity.Member;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class MemberLoginResponse {
+	@NotNull
+	@Schema(description = "회원 id")
 	private final Long id;
+	@NotNull
+	@Schema(description = "닉네임")
 	private final String nickname;
+	@NotNull
+	@Schema(description = "조직")
 	private final String organization;
+	@NotNull
+	@Schema(description = "이메일")
 	private final String email;
+	@NotNull
+	@Schema(description = "가입경로")
 	private final String registrationId;
-	private final String accessToken;
-	private final String refreshToken;
+	@NotNull
+	@Schema(description = "프로필 이미지")
 	private final String profileImage;
+	@NotNull
+	@Schema(description = "Access 토큰")
+	private final String accessToken;
+	@NotNull
+	@Schema(description = "가입경로")
+	private final String refreshToken;
 
 	public MemberLoginResponse(final Member member, final String accessToken, final String refreshToken) {
 		this.id = member.getId();
