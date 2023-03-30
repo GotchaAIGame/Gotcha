1fc6969891365cb9813efbb8ebf59eb0afed75bd
박다솜 <dev.somda@gmail.com>
2023-03-30 12:32:25 +0900

    ✨ feat: 게임 생성  API 적용

--- frontend/src/components/CreateGame/GameCardCarousel.tsx
+++ frontend/src/components/CreateGame/GameCardCarousel.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addProblem } from "@stores/game/gameSlice";
 import GameCard from "./GameCard";
@@ -9,6 +9,8 @@ export default function GameCardCarousel() {
   const gameInfo = useSelector((state: any) => state.game);
   const problemsInfo = gameInfo.problems;
 
+  const [isAddable, setIsAddable] = useState<boolean>(false);
+
   const dispatch = useDispatch();
 
   const addProblemHandler = () => {
@@ -16,25 +18,36 @@ export default function GameCardCarousel() {
     dispatch(addProblem());
   };
 
+  useEffect(() => {
+    return () => {
+    console.log("바뀜")
+    console.log(problemsInfo)
+  }
+  }, [problemsInfo] )
+
   return (
     <div className="cards-and-plusbutton-container">
       {problemsInfo.length &&
         problemsInfo.map((data: any, index: number) => (
           <div key={index}>
-            {/* <p>{index}</p> */}
-            <GameCard index={index} />
+            <GameCard
+              idx={index}
+              isAddable={isAddable}
+              setIsAddable={setIsAddable}
+            />
           </div>
         ))}
       {/* <GameCard /> */}
       {/* <UploadTest /> */}
-
-      <button
-        type="button"
-        onClick={addProblemHandler}
-        className="add-img-button"
-      >
-        +
-      </button>
+      {isAddable && (
+        <button
+          type="button"
+          onClick={addProblemHandler}
+          className="add-img-button"
+        >
+          +
+        </button>
+      )}
     </div>
   );
 }
