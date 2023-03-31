68ea5a374519b0c6fbd42dfbc265d03b64dd35e4
gimgyuyeon <8080claire@naver.com>
2023-03-31 11:20:14 +0900

    ✨ feat: reward api 통신작업

--- frontend/src/components/RankPage/RankButtons.tsx
+++ frontend/src/components/RankPage/RankButtons.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { gamePlayAPI } from "@apis/apis";
 import Button from "@components/common/Button";
 import Modal from "@components/common/Modal";
@@ -6,17 +7,24 @@ import InputBox from "@components/common/InputBox";
 import gold from "@assets/goldmedal.png";
 import silver from "@assets/silvermedal.png";
 import bronze from "@assets/bronzemedal.png";
-import { useSelector } from "react-redux";
+
+interface IReward {
+  grade: number;
+  image: string;
+  nickname: string;
+}
 
 export default function RankButtons() {
   const [modalFiveOpen, setModalFiveOpen] = useState(false);
   const [modalSixOpen, setModalSixOpen] = useState(false);
-  const room = useSelector((state: any) => state.theme.roomId);
+  const [rewardArray, setRewardArray] = useState<IReward[]>([]);
+  const roomId = useSelector((state: any) => state.theme.room);
 
   const handleModalRequest = async () => {
     try {
-      const res = await gamePlayAPI.reward(room);
-      console.log(res.data.result);
+      const res = await gamePlayAPI.reward(roomId);
+      const rewards = res.data.result;
+      setRewardArray(rewards);
     } catch (err) {
       console.error(err);
     }
@@ -73,7 +81,7 @@ export default function RankButtons() {
               src="https://cdn.icoda.co.kr/asset/img_thumbnail/9/1301179_0.jpg"
               alt=""
             />
-            <h5>갤럭시북</h5>
+            <p>갤럭시북</p>
           </div>
           <div className="rank-two">
             <img src={silver} alt="2등" className="medal" />
@@ -81,7 +89,7 @@ export default function RankButtons() {
               src="https://m.etlandmall.co.kr/nas/cdn/attach/product/2021/08/12/S3573918/SYS_2021081211390_0_600.png"
               alt=""
             />
-            <h5>갤럭시버즈</h5>
+            <p>갤럭시버즈</p>
           </div>
           <div className="rank-three">
             <img src={bronze} alt="3등" className="medal" />
@@ -89,20 +97,22 @@ export default function RankButtons() {
               src="https://cdn.icoda.co.kr/asset/img_thumbnail/9/1301179_0.jpg"
               alt=""
             />
-            <h5>스타벅스 아메리카노 Tall</h5>
+            <p>스타벅스 아메리카노 Tall</p>
           </div>
-          <p>
-            <h5 className="h5-text">꼭 알아두세요!</h5>
-            첫구매 쿠폰은 매일 신규 회원 및 구매 이력이 없는 고객만 사용
-            가능합니다. 쿠폰팩은 즉시 발급되며, 쿠폰 사용 기한은 발급 후
-            24시간입니다. 쿠폰팩은 하단의 더.세.페 쿠폰 적용 상품에만
-            적용됩니다. 장바구니 쿠폰은 결제 당 1장 사용 가능하며, 상품쿠폰과
-            장바구니 쿠폰은 동시 적용이 불가합니다.(장바구니 중복쿠폰은 동시
-            적용 가능) 쿠폰 적용 상품은 당사 사정에 의하여 상시 변경될 수
-            있습니다. 해당 쿠폰은 ID당 1일 1회만 발급 가능합니다. APP 전용 특가
-            상품은 장바구니 쿠폰 적용 대상이 아닙니다. 이벤트 기간 주문량 증가로
-            배송 예정일과 실제 배송일 차이가 있을 수 있습니다.
-          </p>
+          <div className="reward-explanation">
+            <p className="h5-text">꼭 알아두세요!</p>
+            <p className="text">
+              첫구매 쿠폰은 매일 신규 회원 및 구매 이력이 없는 고객만 사용
+              가능합니다. 쿠폰팩은 즉시 발급되며, 쿠폰 사용 기한은 발급 후
+              24시간입니다. 쿠폰팩은 하단의 더.세.페 쿠폰 적용 상품에만
+              적용됩니다. 장바구니 쿠폰은 결제 당 1장 사용 가능하며, 상품쿠폰과
+              장바구니 쿠폰은 동시 적용이 불가합니다.(장바구니 중복쿠폰은 동시
+              적용 가능) 쿠폰 적용 상품은 당사 사정에 의하여 상시 변경될 수
+              있습니다. 해당 쿠폰은 ID당 1일 1회만 발급 가능합니다. APP 전용
+              특가 상품은 장바구니 쿠폰 적용 대상이 아닙니다. 이벤트 기간 주문량
+              증가로 배송 예정일과 실제 배송일 차이가 있을 수 있습니다.
+            </p>
+          </div>
         </Modal>
       </div>
     </>
