eef95646d288fb35f0eb433bb74c630e9eebbb40
yezi <yejixheo@gmail.com>
2023-04-02 22:58:30 +0900

    ✅ test : delete reward 테스트 수정

--- backend/src/test/java/org/a602/gotcha/domain/reward/controller/RewardControllerTest.java
+++ backend/src/test/java/org/a602/gotcha/domain/reward/controller/RewardControllerTest.java
@@ -5,7 +5,6 @@ import org.a602.gotcha.CustomSpringBootTest;
 import org.a602.gotcha.domain.member.entity.Member;
 import org.a602.gotcha.domain.reward.entity.Reward;
 import org.a602.gotcha.domain.reward.repository.RewardRepository;
-import org.a602.gotcha.domain.reward.request.DeleteRewardRequest;
 import org.a602.gotcha.domain.reward.request.SetRewardRequest;
 import org.a602.gotcha.domain.reward.request.UpdateRewardRequest;
 import org.a602.gotcha.domain.reward.request.UpdateRewardRequest.UpdateRewardDTO;
@@ -74,10 +73,12 @@ class RewardControllerTest {
         entityManager.persist(room);
         entityManager.persist(saveRoom);
 
-        Reward reward1 = new Reward("리워드 1", 3, room, null);
-        Reward reward2 = new Reward("리워드 2", 4, room, null);
+        Reward reward1 = new Reward("리워드 1", 1, room, null);
+        Reward reward2 = new Reward("리워드 2", 2, room, null);
+        Reward reward3 = new Reward("리워드 3", 3, room, null);
         entityManager.persist(reward1);
         entityManager.persist(reward2);
+        entityManager.persist(reward3);
 
         Member member = Member.builder()
                 .email("suker80@naver.com")
@@ -247,23 +248,12 @@ class RewardControllerTest {
         List<Reward> rewardList = rewardRepository.findByRoomId(room.getId());
         int removeBeforeSize = rewardList.size();
         assertFalse(rewardList.isEmpty());
-        Reward reward = rewardList.get(0);
         mockMvc.perform(delete(url + "/set/reward")
-                .content(objectMapper.writeValueAsString(new DeleteRewardRequest(room.getId(), reward.getId())))
+                .param("roomId", String.valueOf(room.getId()))
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .contentType(MediaType.APPLICATION_JSON));
         int removeAfterSize = rewardRepository.findByRoomId(room.getId()).size();
-        assertEquals(removeBeforeSize - 1, removeAfterSize);
+        assertEquals(removeBeforeSize - 3, removeAfterSize);
     }
 
-    @Test
-    @DisplayName("리워드 삭제 할때 id는 필수 값임")
-    void validRemoveRequest() throws Exception {
-        mockMvc.perform(delete(url + "/set/reward")
-                        .content(objectMapper.writeValueAsString(new DeleteRewardRequest()))
-                        .header(HttpHeaders.AUTHORIZATION, token)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException))
-                .andExpect(status().isBadRequest());
-    }
 }
