cdcae2989b423af16c315c14dde07d23aec4f148
gimgyuyeon <8080claire@naver.com>
2023-04-03 05:22:52 +0900

    🚧 in progress: global CSS 작업중(problemcardlist 오류)

--- frontend/src/pages/EditProfilePage.tsx
+++ frontend/src/pages/EditProfilePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import EditProfile from "@components/EditProfile/EditProfile";
 import "@styles/EditProfilePage.scss";
+import GlobalNavbar from "@components/common/GlobalNavbar";
 
 export default function EditProfilePage() {
   return (
     <div>
+      <GlobalNavbar />
       <EditProfile />
     </div>
   );
--- frontend/src/pages/LogInPage.tsx
+++ frontend/src/pages/LogInPage.tsx
@@ -4,18 +4,12 @@ import LogIn from "@components/Users/LogIn";
 import "@styles/LogInPage.scss";
 import banner from "@assets/TopBanner.svg";
 import logo from "@assets/logo.svg";
+import GlobalNavbar from "@components/common/GlobalNavbar";
 
 export default function LogInPage() {
   return (
     <div className="login-page-container">
-      <div className="top-banner">
-        <img src={banner} alt="" />
-      </div>
-
-      {/* 로고 이미지는 차후 커스텀 이미지로 변경할 예정 */}
-      <Link to="/">
-        <img src={logo} alt="" className="login-banner" />
-      </Link>
+      <GlobalNavbar />
       <LogIn />
     </div>
   );
