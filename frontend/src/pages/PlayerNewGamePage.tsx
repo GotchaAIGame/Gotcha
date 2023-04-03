97504867c836b2a823b3fc17a90c5915875a8189
veonico <veonico@naver.com>
2023-04-03 14:38:33 +0900

    🐛 fix : Timer fixed

--- frontend/src/pages/PlayerNewGamePage.tsx
+++ frontend/src/pages/PlayerNewGamePage.tsx
@@ -1,19 +1,13 @@
-import React  from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import NewPlayerInfo from "@components/PlayerMain/NewPlayerInfo";
 import "@styles/PlayerNewGamePage.scss";
 
 export default function PlayerNewGamePage() {
-  const location = useLocation()
+  const location = useLocation();
   const themeColor = useSelector((state: any) => state.theme.themeColor);
   const themeLogo = useSelector((state: any) => state.theme.themeLogo);
-
-  const tempHandler = () => {
-    // dispatch(registerUser({ roomId: 2, nickname: "veomchan", password: 1234 }));
-    // navigate(`/game/${location.state.inputPin}`, { state: { roomId: 2, authorized: true } });
-    console.log("눌림")
-  };
 
   return (
     <div
@@ -25,10 +19,10 @@ export default function PlayerNewGamePage() {
           <img src={themeLogo} alt="로고" />
         </Link>
       </header>
-      <NewPlayerInfo roomPin={location.state.inputPin} roomId={location.state.room}/>
-      <button type="button" onClick={tempHandler}>
-        실험
-      </button>
+      <NewPlayerInfo
+        roomPin={location.state.inputPin}
+        roomId={location.state.room}
+      />
     </div>
   );
 }
