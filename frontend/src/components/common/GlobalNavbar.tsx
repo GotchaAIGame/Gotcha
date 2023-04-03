ce43cb29c202e0b0796fc30b9fab8d491597ce77
gimgyuyeon <8080claire@naver.com>
2023-04-04 00:54:13 +0900

    🐛 fix: logo 랜더링 해결

--- frontend/src/components/common/GlobalNavbar.tsx
+++ frontend/src/components/common/GlobalNavbar.tsx
@@ -11,27 +11,17 @@ export default function GlobalNavbar() {
   // 해당 링크 참고: https://changmin.tistory.com/40
   const location = useLocation();
 
-  // 보다 명확한 경로 구분이 필요하다면 'path-to-regexp' 라이브러리를 설치해서 사용할 것
-  // if (
-  //   location.pathname === "/login" ||
-  //   location.pathname === "/main" ||
-  //   location.pathname.startsWith("/newgame") || // /newgame으로 시작하는 경로(새게임페이지)
-  //   location.pathname.startsWith("/rejoin") || // /rejoin 시작하는 경로(재참여페이지)
-  //   location.pathname.startsWith("/game") || // /game 시작하는 경로(게임페이지)
-  //   location.pathname.startsWith("/custom") || // /custom 시작하는 경로(커스텀 페이지)
-  //   location.pathname.endsWith("/rank") // /rank으로 끝나는 경로(랭킹페이지)
-  // ) {
-  //   return null;
-  // }
+  let hasLogo = true;
+  if (
+    location.pathname === "/create/game" ||
+    location.pathname.startsWith("/edit")
+  )
+    hasLogo = false;
+
   return (
     <header className="global-nav-header">
       {/* 게임 생성 페이지에서는 로고를 생략합니다. */}
-      <Link to="/">
-        {location.pathname !== "/create/game" ||
-          (location.pathname.startsWith("/edit") && (
-            <img src={logo} alt="logo" />
-          ))}
-      </Link>
+      <Link to="/">{hasLogo ? <img src={logo} alt="logo" /> : ""}</Link>
     </header>
   );
 }
