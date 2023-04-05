453fec45af7f574786836dce883ae6bc9ff5b3fb
gimgyuyeon <8080claire@naver.com>
2023-04-05 09:52:38 +0900

    ✨ Hamburgerbar 로그아웃, 마이페이지 추가

--- frontend/src/components/common/Hambugerbar.tsx
+++ frontend/src/components/common/Hambugerbar.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./styles/Hamburgerbar.scss";
+import { Cookies, useCookies } from "react-cookie";
+import { memberAPI } from "@apis/apis";
+import { useDispatch } from "react-redux";
+import { setLogout } from "@stores/users/userSlice";
 
 export default function Hambugerbar() {
+  const [cookies, setCookie] = useCookies(["refreshToken"]);
+
+  const nickname = useSelector((state: any) => state.users.nickname);
+  const token = sessionStorage.getItem("accessToken");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const menuOnClick = () => {
     const menuBar = document.getElementById("menu-bar");
     const nav = document.getElementById("nav");
@@ -11,6 +23,36 @@ export default function Hambugerbar() {
       menuBar.classList.toggle("change");
       nav.classList.toggle("change");
       menuBg.classList.toggle("change-bg");
+    }
+  };
+
+  const userCheck = () => {
+    console.log(nickname);
+    if (token) {
+      navigate(`/mypage/${nickname}`);
+    } else {
+      navigate("/login");
+    }
+  };
+
+  const logoutHandler = () => {
+    const accessToken = sessionStorage.getItem("accessToken");
+    const { refreshToken } = cookies;
+
+    if (accessToken && refreshToken) {
+      // API 임시 제거
+      const result = memberAPI.logOut(accessToken, refreshToken);
+      result
+        .then(() => {
+          dispatch(setLogout());
+          sessionStorage.setItem("accessToken", "");
+          setCookie("refreshToken", "");
+          alert("다음에 또 봐요!");
+          navigate("/main");
+        })
+        .catch((res) => {
+          console.log(res);
+        });
     }
   };
 
@@ -32,16 +74,19 @@ export default function Hambugerbar() {
         <nav className="nav" id="nav">
           <ul>
             <li>
-              <Link to="/main">Home</Link>
+              <button type="button" onClick={() => navigate("/main")}>
+                Main
+              </button>
             </li>
             <li>
-              <Link to="/mypage">My Page</Link>
+              <button type="button" onClick={userCheck}>
+                My Page
+              </button>
             </li>
             <li>
-              <Link to="/mypage">Login</Link>
-            </li>
-            <li>
-              <Link to="/mypage">Logout</Link>
+              <button type="button" onClick={logoutHandler}>
+                Logout
+              </button>
             </li>
           </ul>
         </nav>
