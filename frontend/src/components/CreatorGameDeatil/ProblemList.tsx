d70e143c64189bac4ae36955f1bcbebf4e3f176e
김범찬 <veonico@naver.com>
2023-04-06 08:57:44 +0900

    Revert "Merge branch 'S08P22A602-16-frontend-sign-up-feature-apply' into 'master'"
    This reverts merge request !167

--- frontend/src/components/CreatorGameDeatil/ProblemList.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ProblemList() {
-  return (
-    <div>ProblemList</div>
-  )
-}
--- frontend/src/pages/ErrorPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ErrorPage() {
-  return (
-    <div>ErrorPage</div>
-  )
-}
