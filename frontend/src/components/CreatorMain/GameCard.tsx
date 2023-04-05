16f550a37c038cceb9cafe12e69d30f462825524
박다솜 <dev.somda@gmail.com>
2023-04-05 13:43:30 +0900

    ✨ feat: 출제 메인 페이지 카드에서 기간 조회

--- frontend/src/components/CreatorMain/GameCard.tsx
+++ frontend/src/components/CreatorMain/GameCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "@components/common/Button";
 import { useDispatch } from "react-redux";
@@ -6,6 +6,7 @@ import { setTheme } from "@stores/player/themeSlice";
 
 export default function GameCard(props: any) {
   const { gameInfo } = props;
+  const [durationText, setDurationText] = useState<string>("");
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -55,17 +56,35 @@ export default function GameCard(props: any) {
     return `${yearStr}.${monthStr}.${dayStr} ${hourStr}:${minuteStr}`;
   }
 
+  useEffect(() => {
+    if (gameInfo) {
+      const today = new Date();
+      const start = new Date(gameInfo.startTime);
+      const end = new Date(gameInfo.endTime);
+
+      if (start > today) {
+        setDurationText("[시작 전]");
+      }
+      if (start <= today && end >= today) {
+        setDurationText("[진행 중]");
+      }
+      if (end < today) {
+        setDurationText("[종료]");
+      }
+    }
+  }, []);
+
   if (gameInfo) {
     // console.log(gameInfo);
 
     const startDateInfo = formatDate(gameInfo.startTime);
     const endDateInfo = formatDate(gameInfo.endTime);
     const today = new Date();
-    const start = new Date(gameInfo.startTime);
-    const end = new Date(gameInfo.endTime);
+
+    // dateChecker(start, end);
 
     return (
-      <div className="card-wrapper">
+      <div className="creator-main-card-wrapper">
         <button type="button" className="detail-move-button" onClick={goDetail}>
           <header>
             <h5>{gameInfo.title}</h5>
@@ -82,6 +101,9 @@ export default function GameCard(props: any) {
             <div className="card-content-wrapper">
               <div className="card-content-title-wrapper">기간</div>
               <div className="card-aside-wrapper">
+                {durationText && (
+                  <p className="duration-text">{durationText}</p>
+                )}
                 <p>시작 {startDateInfo}</p>
                 <p>종료 {endDateInfo}</p>
               </div>
@@ -105,7 +127,7 @@ export default function GameCard(props: any) {
             color="gray-lime"
             onClick={goEdit}
           />
-          {start > today && end > today ? (
+          {durationText === "[시작 전]" ? (
             <Button
               text="랭킹보기"
               size="small"
