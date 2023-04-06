9925918340a3761431be5a71347992ebca49bb3d
gimgyuyeon <8080claire@naver.com>
2023-04-06 17:55:46 +0900

    🚧 in progress: 카카오 로그인 테스트중

--- frontend/src/components/Users/SocialLogin.tsx
+++ frontend/src/components/Users/SocialLogin.tsx
@@ -2,6 +2,7 @@
 
 import Loading from "@components/common/Loading";
 import { setLogin } from "@stores/users/userSlice";
+import { memberAPI } from "@apis/apis";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
@@ -20,35 +21,39 @@ interface ISNSUser {
 
 // 회원가입 성공시 /edit으로 이동
 
-const SocialLogin = () => {
+export default function SocialLogin() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [cookies, setCookie] = useCookies(["refreshToken"]);
   const [snsUser, setSnsUser] = useState<ISNSUser>();
 
+  const snsLogin = async () => {
+    // useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const registrationId = params.get("registrationId") || "";
+    const accessToken = params.get("access_token") || "";
+
+    console.log(registrationId);
+    console.log(accessToken);
+
+    memberAPI
+      .socialLogin(accessToken, registrationId)
+      .then((res) => {
+        console.log(res);
+        setSnsUser(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        // Loading 컴포넌트 등록 해제 로직
+      });
+  };
+
   useEffect(() => {
-    const params = new URL(window.location.href).searchParams;
-
-    console.log(params);
-    // const nickname = decodeURIComponent(params.get("nickname"));
-    // const email = params.get("email");
-    // const registrationId = params.get("registrationId");
-
-    // if (email && registrationId) {
-    //   dispatch(setLogin({ email, registrationId }));
-    //   navigate("/signup");
-    // } else {
-    //   dispatch(setLogin({ nickname, email, registrationId }));
-    //   const accessToken = params.get("access");
-    //   const refreshToken = params.get("refresh");
-
-    //   sessionStorage.setItem("accessToken", accessToken);
-    //   setCookie("refreshToken", refreshToken);
-    //   navigate("/");
-    // }
+    console.log("여기");
+    snsLogin();
   }, []);
 
   return <Loading />;
-};
-
-export default SocialLogin;
+}
