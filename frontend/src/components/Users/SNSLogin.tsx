fb98047edb660016f08fb356452fee348258fe84
gimgyuyeon <8080claire@naver.com>
2023-04-06 15:45:08 +0900

    ♻️ refactor: 소셜로그인 링크수정

--- frontend/src/components/Users/SNSLogin.tsx
+++ frontend/src/components/Users/SNSLogin.tsx
@@ -14,11 +14,11 @@ export default function SNSLogin() {
         <hr />
       </div>
 
-      <Link to="https://j8a602.p.ssafy.io/api/oauth2/authorization/google">
+      <Link to="https://j8a602.p.ssafy.io/oauth2/authorization/google">
         <Button text="구글 로그인" color="google" />
       </Link>
 
-      <Link to="https://j8a602.p.ssafy.io/api/oauth2/authorization/kakao">
+      <Link to="https://j8a602.p.ssafy.io/oauth2/authorization/kakao">
         <Button text="카카오톡 로그인" color="kakao" />
       </Link>
       <Link to="/signup">
