0f58799dc6170c93c721030132a743dabd2cb616
minsulee <alkwen0996@naver.com>
2023-06-04 23:11:40 +0900

    ♻️ refactor : Member 관련 클래스 불변클래스로 수정

--- backend/src/main/java/org/a602/gotcha/domain/member/response/MemberUpdateResponse.java
+++ backend/src/main/java/org/a602/gotcha/domain/member/response/MemberUpdateResponse.java
@@ -1,40 +1,41 @@
 package org.a602.gotcha.domain.member.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotNull;
-
+import lombok.Builder;
+import lombok.Getter;
 import org.a602.gotcha.domain.member.entity.Member;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
-
 @Getter
-public class MemberUpdateResponse {
-	@NotNull
-	@Schema(description = "회원 id")
-	private final Long id;
-	@NotNull
-	@Schema(description = "닉네임")
-	private final String nickname;
-	@NotNull
-	@Schema(description = "조직")
-	private final String organization;
-	@NotNull
-	@Schema(description = "이메일")
-	private final String email;
-	@NotNull
-	@Schema(description = "가입경로")
-	private final String registrationId;
-	@NotNull
-	@Schema(description = "프로필 이미지")
-	private final String profileImage;
+@Builder
+public final class MemberUpdateResponse {
+    @NotNull
+    @Schema(description = "회원 id")
+    private final Long id;
+    @NotNull
+    @Schema(description = "닉네임")
+    private final String nickname;
+    @NotNull
+    @Schema(description = "조직")
+    private final String organization;
+    @NotNull
+    @Schema(description = "이메일")
+    private final String email;
+    @NotNull
+    @Schema(description = "가입경로")
+    private final String registrationId;
+    @NotNull
+    @Schema(description = "프로필 이미지")
+    private final String profileImage;
 
-	public MemberUpdateResponse(final Member member) {
-		this.id = member.getId();
-		this.nickname = member.getNickname();
-		this.organization = member.getOrganization();
-		this.email = member.getEmail();
-		this.registrationId = member.getRegistrationId();
-		this.profileImage = member.getProfileImage();
-	}
-
+    public static MemberUpdateResponse from(final Member member) {
+        return MemberUpdateResponse.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .organization(member.getOrganization())
+                .registrationId(member.getRegistrationId())
+                .profileImage(member.getProfileImage())
+                .build();
+    }
 }
